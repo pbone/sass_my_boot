26f2f4ab5099cfc3df6a8796b089b5561383d7d0
Patrick Hall <patrickhall@patrickimac2.ibi.com>
2017-05-18 15:20:13 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var config = {
+    bootstrapDir: './bower_components/bootstrap-sass',
+    publicDir: './public',
+    themeRoot: './',
+};
+
+var sassPaths = [
+    './bower_components/bootstrap-sass'
+
+];
+
+gulp.task('css', function() {
+    return gulp.src('./scss/style.scss')
+        .pipe(sass({
+            includePaths: [config.bootstrapDir + '/assets/stylesheets'],
+            outputStyle: 'nested',
+            sourceComments: 'map',
+            includePaths: sassPaths
+        }))
+        .pipe(gulp.dest(config.publicDir + '/css'));
+    //.pipe(gulp.dest(config.themeRoot + 'css'));
+});
+
+gulp.task('css-c', function() {
+    return gulp.src('./scss/style.scss')
+        .pipe(sass({
+            includePaths: [config.bootstrapDir + '/assets/stylesheets'],
+            outputStyle: 'compressed',
+            includePaths: sassPaths
+        }))
+        .pipe(gulp.dest(config.publicDir + '/css'));
+    //.pipe(gulp.dest(config.themeRoot + 'css'));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
+        .pipe(gulp.dest(config.publicDir + '/fonts'));
+    // .pipe(gulp.dest(config.themeRoot + 'fonts'));
+});
+
+gulp.task('default', ['css', 'fonts']);
+
+//gulp prod - for compressed, production ready css
+gulp.task('prod', ['css-c', 'fonts']);
+
+
+
